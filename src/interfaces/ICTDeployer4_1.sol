e57215996aae3a6b6b9336846d9e7b2cd86dbfda
mejango <mejango@protonmail.com>
2025-06-05 19:06:15 -0300

    4_1

---
+++ src/interfaces/ICTDeployer4_1.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IJB721TiersHook} from "@bananapus/721-hook/src/interfaces/IJB721TiersHook.sol";
+import {IJB721TiersHookProjectDeployer} from "@bananapus/721-hook/src/interfaces/IJB721TiersHookProjectDeployer.sol";
+import {IJBController} from "@bananapus/core/src/interfaces/IJBController.sol";
+import {IJBProjects} from "@bananapus/core/src/interfaces/IJBProjects.sol";
+
+import {ICTPublisher4_1} from "./ICTPublisher4_1.sol";
+import {CTSuckerDeploymentConfig} from "../structs/CTSuckerDeploymentConfig.sol";
+import {CTProjectConfig} from "../structs/CTProjectConfig.sol";
+
+interface ICTDeployer4_1 {
+    function PROJECTS() external view returns (IJBProjects);
+    function DEPLOYER() external view returns (IJB721TiersHookProjectDeployer);
+    function PUBLISHER() external view returns (ICTPublisher4_1);
+
+    function deployProjectFor(
+        address owner,
+        CTProjectConfig calldata projectConfigurations,
+        CTSuckerDeploymentConfig calldata deployerConfigurations,
+        IJBController controller
+    )
+        external
+        returns (uint256 projectId, IJB721TiersHook hook);
+
+    function deploySuckersFor(
+        uint256 projectId,
+        CTSuckerDeploymentConfig calldata suckerDeploymentConfiguration
+    )
+        external
+        returns (address[] memory suckers);
+}
